04500a1127b7c60999f9eafaf6ece93f0a404b7a
portapipe <portapipe@users.noreply.github.com>
2018-07-01 11:39:29 +0200

    Create Grocery_crud_plus_model.php

---
+++ application/model/Grocery_crud_plus_model.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * PHP grocery CRUD Plus
+ *
+ * LICENSE
+ *
+ * Grocery CRUD Plus is released with dual licensing, using the GPL v3 (license-gpl3.txt) and the MIT license (license-mit.txt).
+ * You don't have to do anything special to choose one license or the other and you don't have to notify anyone which license you are using.
+ * Please see the corresponding license file for details of these licenses.
+ * You are free to use, modify and distribute this software, but all copyright information must remain.
+ *
+ * @package    	grocery CRUD Plus
+ * @copyright  	Copyright (c) 2018 through today, portapipe
+ * @website    	https://github.com/portapipe/grocery-crud-plus/
+ * @version    	1.0.0
+ * @author     	portapipe
+ */
+
+// ------------------------------------------------------------------------
+
+/**
+ * Grocery CRUD Plus Model
+ *
+ *
+ * @package    	grocery CRUD Plus
+ * @author     	portapipe
+ * @link		http://plus.grocerycrud.com/documentation
+ */
+class Grocery_crud_plus_model  extends CI_Model  {
+
+
+	function __construct()
+    {
+        parent::__construct();
+    }
+
+
+}
