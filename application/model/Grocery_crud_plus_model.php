c7763a0c381bc6f844e6e55fc8a5539da3a1998c
portapipe <portapipe@users.noreply.github.com>
2018-07-01 19:21:01 +0200

    fixed spaces->tabs

--- application/model/Grocery_crud_plus_model.php
+++ application/model/Grocery_crud_plus_model.php
@@ -9,11 +9,11 @@
  * Please see the corresponding license file for details of these licenses.
  * You are free to use, modify and distribute this software, but all copyright information must remain.
  *
- * @package    	grocery CRUD Plus
+ * @package		grocery CRUD Plus
  * @copyright  	Copyright (c) 2018 through today, portapipe
- * @website    	https://github.com/portapipe/grocery-crud-plus/
- * @version    	1.0.0
- * @author     	portapipe
+ * @website		https://github.com/portapipe/grocery-crud-plus/
+ * @version		1.0.0
+ * @author	 	portapipe
  */
 
 // ------------------------------------------------------------------------
@@ -22,8 +22,8 @@
  * Grocery CRUD Plus Model
  *
  *
- * @package    	grocery CRUD Plus
- * @author     	portapipe
+ * @package		grocery CRUD Plus
+ * @author	 	portapipe
  * @link		http://plus.grocerycrud.com/documentation
  */
 if(!class_exists("Grocery_crud_model")) include("Grocery_crud_model.php");
@@ -31,19 +31,19 @@ class Grocery_crud_plus_model  extends Grocery_crud_model  {
 
 
 	function __construct()
-    {
-        parent::__construct();
-        
+	{
+		parent::__construct();
+		
 		$directory = APPPATH.'models/GCPlus_modules/';
 		$files = array_diff(scandir($directory), array('..', '.'));
-        foreach($files as $v){
-	        include("GCPlus_modules/$v");
-        }
-    }
-    
-    function __call($functionName, $args) {
-       if(function_exists($functionName))  
-         return call_user_func_array($functionName, $args);
-    }	
+		foreach($files as $v){
+			include("GCPlus_modules/$v");
+		}
+	}
+	
+	function __call($functionName, $args) {
+	   if(function_exists($functionName))  
+		 return call_user_func_array($functionName, $args);
+	}	
 
 }
